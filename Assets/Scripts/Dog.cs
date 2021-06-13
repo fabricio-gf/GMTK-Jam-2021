3f5f0e96def110af9999f15b1d8418e9b0383b4a
Tiago Marino <tmsilva11@gmail.com>
2021-06-13 01:17:08 -0300

    Dog Idle behaviour

--- Assets/Scripts/Dog.cs
+++ Assets/Scripts/Dog.cs
@@ -1,12 +1,12 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
-public class Dog : MonoBehaviour {
+public class Dog : MonoBehaviour 
+{
     // TODO possibly use a priority queue to allow backup distractions
     private Distraction _target;
     private Transform targetTransform;
-    public Distraction Target {
+    public Distraction Target 
+    {
         get { return _target; }
         set {
             ReduceTargetDogCount();
@@ -39,60 +39,116 @@ public class Dog : MonoBehaviour {
 
     private float time;
 
-    private void Awake() {
+    // Idle Behaviour
+    private float idleTime = 0f;
+    private float maxIdleTime = 5f;
+    private bool isWandering = false;
+    private float wanderChance = 0.5f;
+    private Vector2 currentWanderingDirection;
+
+    private void Awake() 
+    {
         rb = GetComponent<Rigidbody>();
         time = Random.value;
         transform = GetComponent<Transform>();
         leashRenderer = GetComponent<LineRenderer>();
     }
 
-    private void Start() {
+    private void Start() 
+    {
         playerTransform = GetComponent<SpringJoint>().connectedBody.transform;
         playerHipTransform = playerTransform.Find("Skeleton").Find("Hips");
+
+        maxIdleTime += Random.value - 0.5f;
     }
 
-    private void FocusNewTarget() {
-        if (Target == null) {
-            // TODO idle movement
-        } else {
+    private void FocusNewTarget() 
+    {
+        if (Target == null)
+        {
+            IdleBehaviour();
+        }
+        else {
             Target.currentDogs++;
         }
     }
 
-    private void ReduceTargetDogCount() {
+    private void IdleBehaviour()
+    {
+        idleTime = 0f;
+        isWandering = (Random.value <= wanderChance);
+        if (isWandering)
+        {
+            currentWanderingDirection = Random.insideUnitCircle;
+        }
+    }
+
+    private void ReduceTargetDogCount() 
+    {
         if (Target != null) {
             Target.currentDogs--;
         }
     }
 
-    private void FixedUpdate() {
-        if (Target != null) {
+    private void FixedUpdate() 
+    {
+        if (Target != null) 
+        {
             rb.AddForce(transform.forward * (Target.weight * distractionForce), ForceMode.Force);
+        }
+        else
+        {
+            if (isWandering)
+            {
+                rb.AddForce(transform.forward * (1f * distractionForce), ForceMode.Force);
+            }
         }
     }
 
-    private void Update() {
+    private void Update()
+    {
         var dt = flatAnimTimeMultiplier;
-        if (Target != null) {
+        if (Target != null)
+        {
             dt += Target.weight;
+        }
+        else
+        {
+            idleTime += Time.deltaTime;
+            if(idleTime >= maxIdleTime)
+            {
+                IdleBehaviour();
+            }
         }
         time += dt * Time.deltaTime;
 
         rotationPivotTransform.localPosition = Vector3.up * (0.25f * bounceLoop.Evaluate(time));
         rotationPivotTransform.localRotation = Quaternion.Euler(0, 0, 20 * (leftRightTiltLoop.Evaluate(time / 2) - 0.5f));
+        
+        CalculateLookDirection();
+    }
 
-        Transform lookTarget;
-        if (Target == null) {
-            lookTarget = playerTransform;
-        } else {
-            lookTarget = targetTransform;
+    private void CalculateLookDirection()
+    {
+        Vector3 lookTarget;
+        if (Target == null)
+        {
+            const float distance = 10f;
+            lookTarget = isWandering ?
+                transform.position + new Vector3(currentWanderingDirection.x, 0f, currentWanderingDirection.y) * distance :
+                playerTransform.position;
         }
-        var lookDir = lookTarget.position - transform.position;
+        else
+        {
+            lookTarget = targetTransform.position;
+        }
+        var lookDir = lookTarget - transform.position;
         var rot = Mathf.Atan2(lookDir.x, lookDir.z) * Mathf.Rad2Deg;
         transform.rotation = Quaternion.Euler(0, rot, 0);
     }
 
-    private void LateUpdate() {
+    private void LateUpdate() 
+    {
         leashRenderer.SetPositions(new Vector3[] {leashAttachmentPoint.position, playerHipTransform.position});
     }
 }
